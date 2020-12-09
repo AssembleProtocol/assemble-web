eddf374ac2312d6ece3288d2bc45ac5ff3ee7084
Cheolho Kim <cheolho@starshell.co>
2020-12-09 11:20:41 +0900

    그래프 예제

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'object-curly-newline': 0,
+    'max-len': 0,
   },
 };
